fdd8404853841d441a0b87a2fb90c39d2e17f358
renya-create <wadarenya1221@gmail.com>
2021-08-24 01:02:50 +0900

    反転させて足し算
    3-4できました！！

--- 3-4/main.js
+++ 3-4/main.js
@@ -1,19 +1,6 @@
 
-//文字列にする
-const num1=String(process.argv[2]);
-const num2=String(process.argv[3]);
+const num1=Number(String(process.argv[2]).split("").reverse().join(""));
+const num2=Number(String(process.argv[3]).split("").reverse().join(""));
 
-//文字列を1文字ずつ配列にする
-const arr1=num1.split("");
-const arr2=num2.split("");
-
-//配列の並びを逆にして、配列をつなげる
-const rev1=arr1.reverse().join("");
-const rev2=arr2.reverse().join("");
-
-//文字列→数値
-const a=Number(rev1);
-const b=Number(rev2);
-
-console.log(a+b);
+console.log(num1+num2);
 
